808917699b47b838c4f6cb0b7cbb4d1a8c04653c
Hugh <hugh7568@163.com>
2022-11-16 23:52:23 +0800

    fix:域名信息转换方法
    1. 域名信息转换方法

--- ddns/src/main/java/com/hugh7568/ddns/converter/DomainConverter.java
+++ ddns/src/main/java/com/hugh7568/ddns/converter/DomainConverter.java
@@ -2,8 +2,8 @@ package com.hugh7568.ddns.converter;
 
 import com.aliyun.alidns20150109.models.DescribeDomainRecordsResponseBody;
 import com.hugh7568.ddns.bean.po.Domain;
-import com.hugh7568.ddns.enums.DomainLevel;
 import com.hugh7568.ddns.enums.DomainType;
+import com.hugh7568.ddns.utils.DomainUtil;
 
 /**
  * 类型转换器
@@ -11,6 +11,8 @@ import com.hugh7568.ddns.enums.DomainType;
  * @author Hugh
  */
 public class DomainConverter {
+
+    private static final String SPOT = ".";
 
     /**
      * 域名信息转换
@@ -20,13 +22,17 @@ public class DomainConverter {
      */
     public static Domain domainConverter(DescribeDomainRecordsResponseBody.DescribeDomainRecordsResponseBodyDomainRecordsRecord record) {
         Domain domain = new Domain();
-        domain.setDomainType(DomainType.A);
-        domain.setDomainName(record.getRR() + "." + record.getDomainName());
+
+        domain.setIsUpdate(Boolean.TRUE);
+        domain.setDomainType(DomainType.getByName(record.getType()));
+
+        domain.setDomainName(record.getDomainName());
+        domain.setSecondDomainName(record.getRR());
         domain.setProviderId(record.getRecordId());
-        domain.setIsUpdate(true);
-        domain.setDomainLevel(DomainLevel.SECOND);
         domain.setDomainAddress(record.getValue());
+
+        //无法区分三级四级域名,所有拼接到一起重新解析等级
+        domain.setDomainLevel(DomainUtil.getDomainLevel(record.getRR() + SPOT + record.getDomainName()));
         return domain;
     }
-
 }
